23955209113403344c001d0caa2b55c1cb6ed30f
Farrux <fboqiyev1065@gmail.com>
2026-06-23 20:36:50 +0500

    New Console line added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome new branch");
         System.out.println("It is easy");
+        System.out.println("Hello");
     }
 
 }
