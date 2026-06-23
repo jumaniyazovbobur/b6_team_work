41d98d97a50261c6fc08aa872ef16a98980038b2
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-06-23 20:26:07 +0500

    ProfileCar entity created

---
+++ src/entities/ProfileCar.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class ProfileCar {
+    private String id;
+    private String profileName;
+    private String carId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+}
